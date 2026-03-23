158501e235627ddcdcf070b5c6d2360a1cb45c7c
IGENERALTKS <olimpio.h2so4@gmail.com>
2026-03-23 16:55:17 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')).render(
   //roda duas vezes o projeto
   <StrictMode> 
+    <SpeedInsights/>
+    <Analytics/>
     <App />
   </StrictMode>,
 )
